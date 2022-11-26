54a2160bee87462456ae3d655c6310de701c24fe
mdmafuz2022 <golam.mafuz.work2@gmail.com>
2022-11-26 13:55:53 +0530

    Learning Strings

---
+++ lists.py
@@ -0,0 +1,7 @@
+# A list of numbers
+list_of_numbers = [4, 5, 6]
+# We can have a list of stings 'd', 'e', 'f' -- Note strings are in quotes
+list_of_string = ['d', 'e', 'f']
+
+print("List of Numbers : {}".format(list_of_numbers))
+print("List of Strings : {}".format(list_of_string))
